6d255e52bf3d3be9c6264e981f717c17012721e0
Javier Guerrero <steven.guesp@gmail.com>
2019-06-20 00:55:56 -0400

    UPdate html

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -10,4 +10,3 @@ ReactDOM.hydrate(
   </Router>, 
   document.getElementById('app')
 );
-
--- src/client/js/react/components/App.jsx
+++ src/client/js/react/components/App.jsx
@@ -2,24 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import routes from '_routes/appRoutes';
 import './App.scss';
-import HomePage from './HomePage';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-  }
-  componentDidMount() {
-    console.log("Application has mounted on the browser!");
-  }
-  render() {
-    return (
-        <Switch>
-          {routes.map((route, index) => (
-            <Route key={`${route.path ? route.path : `404Page`}-${index}`} {...route} />
-          ))}
-        </Switch>
-    );
-  }
-}
+const App = () => (
+  <Switch>
+    {routes.map((route, index) => (
+      <Route key={`${route.path ? route.path : `404Page`}-${index}`} {...route} />
+    ))}
+  </Switch>
+)
 
 export default App;
